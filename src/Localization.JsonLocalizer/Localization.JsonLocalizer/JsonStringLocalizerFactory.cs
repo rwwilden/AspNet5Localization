e674d70c2d246c669a2fa0363277bbea7ffebace
Ronald Wildenberg <rwildenberg@itq.nl>
2016-02-28 17:38:06 +0100

    Implemented JsonStringLocalizerFactory.Create(string, string) method so injected IViewLocalizers also work

--- src/Localization.JsonLocalizer/Localization.JsonLocalizer/JsonStringLocalizerFactory.cs
+++ src/Localization.JsonLocalizer/Localization.JsonLocalizer/JsonStringLocalizerFactory.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Concurrent;
+using System.Linq;
 using System.IO;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -12,6 +13,8 @@ namespace Localization.JsonLocalizer.StringLocalizer
 {
     public class JsonStringLocalizerFactory : IStringLocalizerFactory
     {
+        private static readonly string[] KnownViewExtensions = new[] { ".cshtml" };
+        
         private readonly ConcurrentDictionary<string, JsonStringLocalizer> _localizerCache =
             new ConcurrentDictionary<string, JsonStringLocalizer>();
         
@@ -64,14 +67,15 @@ namespace Localization.JsonLocalizer.StringLocalizer
             var typeInfo = resourceSource.GetTypeInfo();
             var assembly = typeInfo.Assembly;
 
-            // Re-root the base name if a resources path is set
-            var baseName = string.IsNullOrEmpty(_resourcesRelativePath)
+            // Re-root the base name if a resources path is set.
+            var resourceBaseName = string.IsNullOrEmpty(_resourcesRelativePath)
                 ? typeInfo.FullName
-                : _applicationEnvironment.ApplicationName + "." + _resourcesRelativePath
-                    + LocalizerUtil.TrimPrefix(typeInfo.FullName, _applicationEnvironment.ApplicationName + ".");
-            _logger.LogVerbose($"Localizer basename: {baseName}");
+                : _applicationEnvironment.ApplicationName + "." + _resourcesRelativePath +
+                    LocalizerUtil.TrimPrefix(typeInfo.FullName, _applicationEnvironment.ApplicationName + ".");
+            _logger.LogVerbose($"Localizer basename: {resourceBaseName}");
 
-            return _localizerCache.GetOrAdd(baseName, _ => new JsonStringLocalizer(baseName, _applicationEnvironment.ApplicationName, _logger));
+            return _localizerCache.GetOrAdd(
+                resourceBaseName, new JsonStringLocalizer(resourceBaseName, _applicationEnvironment.ApplicationName, _logger));
         }
 
         public IStringLocalizer Create(string baseName, string location)
@@ -82,7 +86,22 @@ namespace Localization.JsonLocalizer.StringLocalizer
             }
             
             _logger.LogVerbose($"Getting localizer for baseName {baseName} and location {location}.");
-            throw new NotImplementedException();
+            
+            location = location ?? _applicationEnvironment.ApplicationName;
+            
+            // Re-root base name if a resources path is set and strip the cshtml part.
+            var resourceBaseName = location + "." + _resourcesRelativePath + LocalizerUtil.TrimPrefix(baseName, location + ".");
+            
+            var viewExtension = KnownViewExtensions.FirstOrDefault(extension => resourceBaseName.EndsWith(extension));
+            if (viewExtension != null)
+            {
+                resourceBaseName = resourceBaseName.Substring(0, resourceBaseName.Length - viewExtension.Length);
+            }
+            
+            _logger.LogVerbose($"Localizer basename: {resourceBaseName}");
+            
+            return _localizerCache.GetOrAdd(
+                resourceBaseName, new JsonStringLocalizer(resourceBaseName, _applicationEnvironment.ApplicationName, _logger));
         }
     }
 }
